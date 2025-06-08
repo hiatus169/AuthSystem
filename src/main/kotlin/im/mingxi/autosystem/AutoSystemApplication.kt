c1b78e7c832f8bf26cddb9460ff93b06edb59656
hiatus169 <2968447202@qq.com>
2025-06-08 19:53:46 +0800

    Init Commit

---
+++ src/main/kotlin/im/mingxi/autosystem/AutoSystemApplication.kt
@@ -0,0 +1,11 @@
+package im.mingxi.autosystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AutoSystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<AutoSystemApplication>(*args)
+}
---
+++ src/test/kotlin/im/mingxi/autosystem/AutoSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package im.mingxi.autosystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AutoSystemApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
